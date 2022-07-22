8fc6af2e3f88a14b3bc9f837b356d3174d9e794c
Phu Nguyen <nguyen_van_phu@allgrow-labo.jp>
2022-07-22 15:50:28 +0700

    header menu include reponsive

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,1 +1,24 @@
-console.log("main.js");
+/*===== MENU SHOW/HIDDEN =====*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.querySelector(".c-header__menu");
+
+navToggle.addEventListener("click", () => {
+  navToggle.classList.toggle("is-active");
+  navMenu.classList.toggle("is-showmenu");
+});
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth < 768) {
+    navMenu.classList.remove("is-showmenu");
+    navToggle.classList.remove("is-active");
+  }
+});
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".c-header__link");
+function linkAction() {
+  // When we click on each menu__link, we remove the show-menu class
+  navToggle.classList.toggle("is-active");
+  navMenu.classList.toggle("is-showmenu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
